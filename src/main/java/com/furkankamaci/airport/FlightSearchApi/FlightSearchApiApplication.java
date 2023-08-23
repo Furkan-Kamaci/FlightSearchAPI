1ec67a8a59d215f3ee26a2b75e91eef2c2552dc3
Furkan KAMACI <furkan.kamaci123@gmail.com>
2023-08-24 01:05:11 +0300

    Swagger is implemented

--- src/main/java/com/furkankamaci/airport/FlightSearchApi/FlightSearchApiApplication.java
+++ src/main/java/com/furkankamaci/airport/FlightSearchApi/FlightSearchApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "com.furkankamaci.airport.FlightSearchApi.DataAccess")
 public class FlightSearchApiApplication {
 
 	public static void main(String[] args) {
